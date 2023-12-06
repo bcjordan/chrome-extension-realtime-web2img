3bbc90592a6a4e42d44983c0312951d6b04bae9c
Brian Jordan <206973+bcjordan@users.noreply.github.com>
2023-12-06 10:28:20 -0500

    Add fal AI Chrome extension sample

--- src/content.js
+++ src/content.js
@@ -3,18 +3,126 @@ import './content.css'
 
 const html = `
 <div class="crx">
-  <img src="${chrome.runtime.getURL(src)}" />
+  <img id="preview" src="${chrome.runtime.getURL(src)}" />
 <!--  <img src="https://vitejs.dev/logo.svg" />-->
 </div>
 `
 
 const doc = new DOMParser().parseFromString(html, 'text/html')
 document.body.append(doc.body.firstElementChild)
+const previewRef = document.getElementById('preview');
+
+previewRef.style.width = '512px';
+previewRef.style.height = '512px';
+previewRef.style.opacity = '1';
+
+previewRef.onclick = () => {
+  // toggle vis
+  console.log(previewRef.style.opacity)
+  previewRef.style.opacity = previewRef.style.opacity === '1' ? '.25' : '1';
+  previewRef.style.width = previewRef.style.width === '512px' ? '256px' : '512px';
+  previewRef.style.height = previewRef.style.height === '512px' ? '256px' : '512px';
+}
 
 let n = 0;
+
+function snapVideo(el) {
+  var targetHeight;
+  var targetWidth;
+  var resizeCanvas = document.createElement("canvas");
+  resizeCanvas.width = 512;
+  resizeCanvas.height = 512;
+  var ctx = resizeCanvas.getContext('2d');
+  // ctx.drawImage(el, 0,0, el.videoWidth, el.videoHeight,0,0, 512, 512);
+  // calculate aspect ratio
+  var aspectRatio = el.videoWidth / el.videoHeight;
+  if (el.videoWidth > el.videoHeight) {
+    // landscape
+    targetWidth = resizeCanvas.width;
+    targetHeight = resizeCanvas.width / aspectRatio;
+  } else {
+    // portrait
+    targetWidth = resizeCanvas.height * aspectRatio;
+    targetHeight = resizeCanvas.height;
+  }
+  // center the image
+  var targetX = resizeCanvas.width / 2 - targetWidth / 2;
+  var targetY = resizeCanvas.height / 2 - targetHeight / 2;
+  // draw the image
+  ctx.drawImage(el, targetX, targetY, targetWidth, targetHeight);
+
+  var du = resizeCanvas.toDataURL('image/png')
+  // destroy
+  resizeCanvas.remove()
+  return du;
+
+
+}
+
+function snapCanvas(canvasElement) {
+  // const originalWidth = canvasElement.width;
+  // const originalHeight = canvasElement.height;
+  canvasElement.preserveDrawingBuffer = true;
+  canvasElement.willReadFrequently = true;
+
+  // canvasElement.width = 512;
+  // canvasElement.height = 512;
+  // canvasElement.style.height = '512px';
+  // canvasElement.style.width = '512px';
+  // canvasElement.style.transform = `rotate(${++n}deg)` // they will know us by our continuous rotation
+  // let url = canvasElement.toDataURL('image/png');
+
+  var resizedCanvas = document.createElement("canvas");
+  var resizedContext = resizedCanvas.getContext("2d");
+
+  resizedCanvas.height = "512";
+  resizedCanvas.width = "512";
+
+  // var canvas = document.getElementById("original-canvas");
+
+  resizedContext.drawImage(canvasElement, 0, 0, canvasElement.width, canvasElement.height, 0,0, 512, 512);
+  var myResizedData = resizedCanvas.toDataURL();
+  resizedCanvas.remove()
+
+  // restore
+  // canvasElement.width = originalWidth;
+  // canvasElement.height = originalHeight;
+  // canvasElement.style.height = `${originalHeight}px`;
+  // canvasElement.style.width = `${originalWidth}px`;
+  return myResizedData;
+}
+
 let rotate = () => {
-  const el = document.querySelector('canvas')
-  el.style.transform = `rotate(${++n}deg)`
+  const canvasElement = document.querySelector('canvas')
+  // console.log(el)
+  // el.width = 512;
+  // el.height = 512;
+
+
+  const videoElement = document.querySelector('video')
+
+  if (n % 60 === 0 && (!!videoElement || !!canvasElement)) {
+    window.canvas = canvasElement;
+
+    const imageUrl = videoElement ? snapVideo(videoElement) : snapCanvas(canvasElement);
+
+    console.log(imageUrl);
+    connection.send({
+      // snag a canvas' data URL usng e.g. threeJSRenderer.domElement.toDataURL("image/png"); or canvas.toDataURL("image/png");
+      // NOTE: ensure your input is 512x512. Other sizes may negatively impact inference performance.
+      image_url: imageUrl,
+      sync_mode: true,
+      // Mess with prompt here, text box would be awesome (could even go in main.js popup instead of content)
+      prompt:     `colorful bright icy mountain glittering powder 4k top amazing`,
+
+      strength: 0.65,
+      num_inference_steps: 8,
+      seed: 1234,
+      // if you want it to not have consistency across frames / re-roll dice:
+      // seed: Math.abs(Math.floor(Math.random() * 100000)),
+      enable_safety_checks: false,
+    })
+  }
 
   requestAnimationFrame(rotate);
 };
@@ -22,3 +130,73 @@ requestAnimationFrame(rotate);
 // var canvas = document.querySelector("canvas");
 // // get the context
 // console.log(canvas);
+
+// First, auth client with fal
+// WARNING: You shouldn't do this from the web client directly as it exposes your fal API key.
+//    To offer to web visitors you should auth with backend using their API (https://github.com/tldraw/tldraw does this)
+//        see https://github.com/fal-ai/serverless-js#the-fal-client-proxy for sample of how to hook up with e.g. nextjs on vercel as backend
+//        this won't impact realtime performance (it just passes a token during socket auth instead)
+import * as fal from "@fal-ai/serverless-client";
+fal.config({
+  credentials: "MY_LONG_FAL_AI_KEY" // get at https://www.fal.ai/dashboard/keys
+  // track usage / set limits at https://www.fal.ai/dashboard/usage
+})
+
+// Connect to fal realtime endpoint. As of December 4th, 2023, this is the fastest.
+const connection = fal.realtime.connect('110602490-lcm-plexed-sd15-i2i', {
+  connectionKey: 'realtime-connection-key',
+  clientOnly: false,
+  // if you want to use fal's default throttling, you might be able to just raise this value.
+  //   but see below for sample of adaptive request timing (feels better for partially-interactive continuous i2i)
+  throttleInterval: 250,
+  onError: (error) => {
+    // Re-set-up needed? Not sure
+    console.error(error)
+  },
+  onResult: (result) => {
+    if (result.images && result.images[0]) {
+      let imageURL = result.images[0].url;
+      console.log(imageURL) // - this is your image, base64 encoded jpeg
+      // Set to #preview
+      document.querySelector('#preview').src = imageURL;
+
+      // for my drawing app I aiImageQueue.push new images into a queue/aiImageQueue.shift from a queue so they don't "bunch up" when I render
+      // aiImageQueue.push(result.images[0].url);
+    }
+  },
+});
+
+// Send image from canvas to fal
+
+
+//
+// advanced stuff for perf juicers, accurate as of December 4th, 2023
+//
+
+// Calculates # of steps to run with gradual up-scaling.
+// IMPORTANT: be sure to use dynamic timeouts like with getTimeoutForRequests() if using this technique
+
+// If you just want non-interactive realtime or don't care about juicing later quality,
+//    just return a constant # of steps instead (start low to figure out highest framerate possible)
+//    getTimeoutForRequests() is a good starting point for ideal request delay though maybe conservative at
+//    higher qualities
+//       (things may evolve as fal optimizes inference speed/capacity further)
+function numStepsToRun() {
+  let secondsSinceInteraction = (new Date() - lastLowQualityTime) / 1000;
+  // start at 2 steps, max 12 steps (reached after 24 seconds)
+  return 2 + Math.min(10, Math.floor(secondsSinceInteraction / 2));
+}
+
+// If you make too many requests to fal.ai currently, it will:
+//  (1) increase input lag due to the socket's requests getting queued up
+//        (bc inference is round-robin processed fal-side)
+//  (2) increase the chance of LOWER framerate/request timeouts
+//        (bc "remove old requests to keep it realtime" logic fal-side)
+//    so you want your requests to be carefully tuned to the current fastest available speed
+//      (maybe this could be detected automatically in the future or provided by fal as a hint)
+function getTimeoutForRequests() {
+  // UNUSED - use if you want to throttle based on interaction, or as a rule of thumb for parallel request rate
+  // wait 100ms after 2-step, wait 1700ms after 12-step
+  //    the 70ms additional per extra step is probably too conservatively high, haven't dialed that in
+  return 100 + ((numStepsToRun() - 2) * 70);
+}
